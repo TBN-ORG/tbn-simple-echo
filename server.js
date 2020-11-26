cf97b761edd2359a8c00969f1a08a488a5ff79ef
rfinner <rfinneran@tbn.tv>
2020-11-26 00:22:38 -0800

    Created Dockerfile

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
-var http = require('http');
-http.createServer(function(request,response){
-    response.writeHead(200);
-    response.write("echo: ");
-    request.pipe(response);
-}).listen(8080);
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
